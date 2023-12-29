8e8b9849a0ed4af94b80db753260c6212d1064c6
Rob Murrer <robmurrer@gmail.com>
2023-12-28 20:34:12 -0600

    canvas working, clamping wierd still annd no header

--- canvas_io.py
+++ canvas_io.py
@@ -7,12 +7,11 @@ def get_canvas_np(width: int, height: int)->np.ndarray:
     return np.zeros((width,height,3), dtype=np.double)
 
 def get_canvas(width: int, height: int)->canvas:
-    return [[[0 for i in range(3)] for j in range(width)] for k in range(height)]
+    return [[[0 for i in range(3)] for j in range(height)] for k in range(width)]
 
-CAP_COLOR = 255
 def canvas_write_pixel(canvas: canvas, x: int, y: int, color: pv.tuple3):
-    assert(x < len(canvas))
     assert(y < len(canvas[0]))
+    assert(x < len(canvas))
     assert(x >=0 and y >=0)
 
     canvas[x][y] = color
@@ -30,6 +29,18 @@ def canvas_to_ppm_str(canvas: canvas)->str:
     colors = len(canvas[0][0])
     assert(colors == 3)
 
+    # really slow/weird without numpy?
+    c = np.array(canvas).reshape(-1)
+
+    # need to clamp to 0 to 255
+    max = np.max(c)
+    min = np.min(c)
+    #assert(min >= 0)
+    CAP_COLOR = 255
+    c = np.clip(c, 0, CAP_COLOR)
+    c = CAP_COLOR*(c / max)
+    
+    return c
 
 def canvas_np_to_ppm_str(canvas: np.ndarray)->str:
     pass
@@ -43,6 +54,17 @@ def test_Canvas():
     canvas_write_pixel(c, 65, 75, (1,2,3))
     assert(pv.float_is_equal(c[65][75][2], 3))
 
-    print("Canvas Tests Pass")
+    c = get_canvas(5,5)
+    canvas_write_pixel(c, 1, 1, (1,2,3))
+    canvas_write_pixel(c, 0, 0, (1,1,1))
+    cstr = canvas_to_ppm_str(c)
 
+    c = get_canvas(5,3)
+    canvas_write_pixel(c, 0, 0, (1.5,0,0))
+    canvas_write_pixel(c, 2, 1, (0, 0.5, 0))
+    canvas_write_pixel(c, 4, 2, (-0.5, 0, 1))
+    cstr = canvas_to_ppm_str(c)
+    print(cstr)
+
+    print("Canvas Tests Pass")
 test_Canvas()
