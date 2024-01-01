d2a2e5ea3fb56cf7e2a527e2fcd4d220db4fb0ca
Rob Murrer <robmurrer@gmail.com>
2023-12-31 20:33:49 -0600

    copilot made short work of this writing file to disk

--- canvas_io.py
+++ canvas_io.py
@@ -39,20 +39,35 @@ def canvas_to_ppm_str(canvas: canvas)->str:
     CAP_COLOR = 255
     c = np.clip(c, 0, CAP_COLOR)
     c = CAP_COLOR*(c / max)
-    
-    return c
 
-def canvas_np_to_ppm_str(canvas: np.ndarray)->str:
-    pass
+    import os 
+    out_str = "P3" + os.linesep 
+    out_str = out_str + str(width) + " " + str(height) + os.linesep #copilot
+    out_str = out_str + str(CAP_COLOR) + os.linesep #copilot
+    for i in range(len(c)):
+        out_str = out_str + str(int(c[i])) + " "
+        if (i+1) % (colors*width) == 0:
+            out_str = out_str + os.linesep #copilot
+
+    return out_str 
+
+def canvas_to_ppm(canvas: canvas, filename: str): #copilot
+    with open(filename, "w") as f:
+        f.write(canvas_to_ppm_str(canvas))
  
 def test_Canvas():
     c = get_canvas(100,100)
     canvas_write_pixel(c, 50, 50, (1,2,3))
     assert(pv.float_is_equal(c[50][50][0], 1))
     assert(pv.float_is_equal(c[50][50][2], 3))
+    canvas_write_pixel(c, 51, 51, (1,2,3))
+    canvas_write_pixel(c, 52, 52, (1,2,3))
 
     canvas_write_pixel(c, 65, 75, (1,2,3))
     assert(pv.float_is_equal(c[65][75][2], 3))
+
+    # turn off cause it would hit disk every load
+    #canvas_to_ppm(c, "test.ppm")
 
     c = get_canvas(5,5)
     canvas_write_pixel(c, 1, 1, (1,2,3))
@@ -64,7 +79,8 @@ def test_Canvas():
     canvas_write_pixel(c, 2, 1, (0, 0.5, 0))
     canvas_write_pixel(c, 4, 2, (-0.5, 0, 1))
     cstr = canvas_to_ppm_str(c)
-    print(cstr)
+    #print(cstr)
+
 
     print("Canvas Tests Pass")
 test_Canvas()
